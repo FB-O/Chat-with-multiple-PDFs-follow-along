2f6e98cabdc813157d979c6eeb49982b17ed534e
FQIH-BERRADA <oussama.fqih-berrada@capgemini.com>
2023-11-02 14:53:04 +0100

    feat: added function for splitting text into chunks: get_text_chunks()

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
-import streamlit as st
 from dotenv import load_dotenv
+from langchain.text_splitter import CharacterTextSplitter
 from PyPDF2 import PdfReader
+import streamlit as st
 
 
-def get_pdf_text(pdf_docs):
+def get_pdf_text(pdf_docs) -> str:
     text = ""
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
+
+def get_text_chunks(text: str) -> list[str]:
+    text_splitter = CharacterTextSplitter(
+        separator='\n',
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
 
 
 def main():
@@ -27,6 +40,7 @@ def main():
                 raw_text = get_pdf_text(pdf_docs)
 
                 # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
 
                 # create vector store
 
